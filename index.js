e767a10a7aab89e758a0706b6a55a5fab06ba6b2
junwoo45 <sinyoorang@gmail.com>
2020-03-11 15:52:46 +0900

    style: Draw SVG using D3

---
+++ index.js
@@ -0,0 +1,39 @@
+const a = document.querySelector('div');
+const b = d3.select('div');
+
+const c = document.querySelectorAll('div');
+const d = d3.selectAll('div');
+
+const canvas = d3.select('.canvas');
+
+const svg = canvas.append('svg')
+    .attr('height', 600)
+    .attr('width', 600);
+
+// svg.attr('height', 600);
+// svg.attr('width', 50);
+
+svg.append('rect')
+    .attr("width", 20)
+    .attr("height", 100)
+    .attr("fill", 'blue')
+    .attr("x", 20)
+    .attr("y", 50);
+svg.append('circle')
+    .attr("r", 10)
+    .attr("cx", 400)
+    .attr("cy", 300)
+    .attr("fill", 'pink');
+svg.append('line')
+    .attr("x1", 10)
+    .attr("y1", 10)
+    .attr("x2", 300)
+    .attr("y2", 300)
+    .attr("stroke", "green")
+
+console.log('a : ', a);
+console.log('b : ', b);
+console.log('c : ', c);
+console.log('d : ', d);
+console.log('canvas : ', canvas);
+console.log('svg : ', svg);
